e2ff2daa9df883f08cd228b3cdfc5593f7b67eb2
Nahuel-P <pachecoalannahuel@gmail.com>
2021-06-08 21:52:00 -0300

    Agrego carpeta para tests

---
+++ test/ar/edu/unq/po2/alquilerestemporales/UsuarioTestCase.java
@@ -0,0 +1,14 @@
+package ar.edu.unq.po2.alquilerestemporales;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UsuarioTestCase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
