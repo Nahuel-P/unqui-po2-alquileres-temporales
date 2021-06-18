6030872b14b483fd384c4635470bfe430cd282a1
Nahuel-P <pachecoalannahuel@gmail.com>
2021-06-17 23:04:16 -0300

    Agrego tests de Usuario. Implemento setters y getters de usuario

--- src/ar/edu/unq/po2/alquilerestemporales/Usuario.java
+++ src/ar/edu/unq/po2/alquilerestemporales/Usuario.java
@@ -1,5 +1,52 @@
 package ar.edu.unq.po2.alquilerestemporales;
 
-public class Usuario {
+import java.time.LocalDate;
 
+public class Usuario {
+	
+	
+	private String nombreYApellido;
+	private String direccion;
+	private String telefono;
+	private LocalDate fechaDeAlta;
+
+	public Usuario(String nombreYApellido, String direccion, String telefono, LocalDate fechaDeAlta) {
+		this.setNombreYApellido(nombreYApellido);
+		this.setDireccion(direccion);
+		this.setTelefono(telefono);
+		this.fechaDeAlta = fechaDeAlta;
+	}
+
+	public Usuario() {
+		this("", "", "", LocalDate.now());
+	}
+
+	public String getNombreYApellido() {
+		return this.nombreYApellido;
+	}
+	
+	public String getDireccion() {
+		return this.direccion;
+	}
+	
+	public String getTelefono() {
+		return this.telefono;
+	}
+
+	public Object getFechaDeAlta() {
+		return fechaDeAlta;
+	}
+
+	public void setNombreYApellido(String nombreYApellidoDeUsuario) {
+		this.nombreYApellido = nombreYApellidoDeUsuario;		
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+		
 }
--- test/ar/edu/unq/po2/alquilerestemporales/UsuarioTestCase.java
+++ test/ar/edu/unq/po2/alquilerestemporales/UsuarioTestCase.java
@@ -2,13 +2,59 @@ package ar.edu.unq.po2.alquilerestemporales;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UsuarioTestCase {
-
+	private String nombreYApellidoDeUsuario;
+	private String nombreYApellidoDeUsuario2;
+	private String direccion;
+	private String telefono;
+	private LocalDate fechaDeAlta;
+	private Usuario usuarioSinDatos;
+	private Usuario usuario;
+	private String direccion2;
+	private String telefono2;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		this.usuarioSinDatos = new Usuario();
+		this.nombreYApellidoDeUsuario = "Pepito Gómez";
+		this.direccion = "Calle Falsa 123";
+		this.telefono = "123654789";
+		this.fechaDeAlta = LocalDate.now();
+		this.usuario = new Usuario(nombreYApellidoDeUsuario, direccion, telefono, fechaDeAlta);
+		this.nombreYApellidoDeUsuario2 = "Pepita Gómez";
+		this.direccion2="Avenida Siempre Viva 742";
+		this.telefono2= "987456321";
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCreation() {
+		assertNotNull(this.usuarioSinDatos);
+	}
+	
+	@Test
+	void testConstructor() {
+		assertNotNull(this.usuario);
+	}
+	
+	@Test
+	void testGetDatos() {
+		assertEquals(this.nombreYApellidoDeUsuario, this.usuario.getNombreYApellido());
+		assertEquals(this.direccion, this.usuario.getDireccion());
+		assertEquals(this.telefono, this.usuario.getTelefono());
+		assertEquals(this.fechaDeAlta, this.usuario.getFechaDeAlta());
+	}
+	
+	@Test
+	void testSetDatos() {
+		this.usuario.setNombreYApellido(this.nombreYApellidoDeUsuario2);
+		this.usuario.setDireccion(this.direccion2);
+		this.usuario.setTelefono(this.telefono2);
+		assertEquals(this.nombreYApellidoDeUsuario2, this.usuario.getNombreYApellido());
 	}
 
 }
