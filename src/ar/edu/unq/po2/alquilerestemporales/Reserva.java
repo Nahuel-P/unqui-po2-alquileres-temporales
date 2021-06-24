4c83a99811ebcb767726ed9af14064468f07f4a0
Nahuel-P <pachecoalannahuel@gmail.com>
2021-06-24 01:02:17 -0300

    Agrego tests de reserva

--- src/ar/edu/unq/po2/alquilerestemporales/Reserva.java
+++ src/ar/edu/unq/po2/alquilerestemporales/Reserva.java
@@ -9,27 +9,62 @@ public class Reserva {
 	private Aceptada estadoDeReserva;
 	private Usuario inquilino;
 	private Publicacion publicacion;
+	private FormaDePago formaDePago;
+	private LocalDate fechaRealizacionDeReserva ;
 	
-	public Reserva(Usuario inquilino, LocalDate fechaIngreso, LocalDate fechaSalida, Aceptada estadoDeReserva, Publicacion publicacion ) {
+	public Reserva(LocalDate fechaRealizacionDeReserva, Usuario inquilino, LocalDate fechaIngreso, LocalDate fechaSalida, Aceptada estadoDeReserva, Publicacion publicacion,FormaDePago formaDePago ) {
+		this.fechaRealizacionDeReserva = fechaRealizacionDeReserva;
 		this.fechaIngreso = fechaIngreso;
 		this.fechaSalida = fechaSalida;
 		this.estadoDeReserva = estadoDeReserva;
 		this.inquilino = inquilino;
 		this.publicacion = publicacion;
+		this.formaDePago = formaDePago;
 	}
 
 
 	public float costoPorDia() {
-		return 0f;
+		return this.costoTotal()/this.getDiferenciaDeDias();
 	}
 
 	public int diasQueFaltan() {
-		return 0;
+		Long periodo = Duration.between(this.getFechaRealizacionDeReserva().atStartOfDay(), this.getFechaDeIngreso().atStartOfDay()).toDays();
+		return periodo.intValue();
 	}
 
-	public float costoTotal() {
-		return 0f;
+	private LocalDate getFechaRealizacionDeReserva() {
+		return this.fechaRealizacionDeReserva;
 	}
+	
+	
+	
+	
+	
+	
+
+
+	public float costoTotal() {
+		float costoTotal = 0.00f;
+		LocalDate dateInicio = this.fechaIngreso;
+		LocalDate dateSalida= this.fechaSalida;
+		for(LocalDate date = dateInicio; date.isBefore(dateSalida); date = date.plusDays(1)) {
+			costoTotal += precioEnElDia(date);
+		}		
+		return costoTotal;
+	}
+
+	public float precioEnElDia(LocalDate dia) {
+		float costo = publicacion.getPrecioBase();
+		for (PrecioTemporal temporada : this.publicacion.getTemporadasEspeciales()) {
+			if(dia.isAfter(temporada.getInicio())
+					&& dia.isBefore(temporada.getFinal())) {
+				costo = (float) temporada.getPrecio();
+			break;
+			}
+		}
+		return costo;
+	}
+
 
 	public Usuario getPropietario() {
 		return this.publicacion.getPropietario();
@@ -39,7 +74,7 @@ public class Reserva {
 		this.estadoDeReserva = aceptada;		
 	}
 
-	public Object getInquilino() {
+	public Usuario getInquilino() {
 		return inquilino;
 	}
 
@@ -55,11 +90,15 @@ public class Reserva {
 
 	public int getDiferenciaDeDias() {	
 		Long periodo = Duration.between(this.getFechaDeIngreso().atStartOfDay(), this.getFechaDeSalida().atStartOfDay()).toDays();
-		return periodo.intValue();
+		return Math.abs(periodo.intValue());
 	}
 
 
 	public Aceptada getEstadoDeReserva() {
 		return this.estadoDeReserva;
+	}
+	
+	public FormaDePago getFormaDePago(){
+		return this.formaDePago;
 	}
 }
--- test/ar/edu/unq/po2/alquilerestemporales/ReservaTestCase.java
+++ test/ar/edu/unq/po2/alquilerestemporales/ReservaTestCase.java
@@ -6,6 +6,8 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,18 +21,26 @@ class ReservaTestCase {
 	private Usuario inquilino;
 	private Publicacion publicacion;
 	private Usuario propietario;
+	private FormaDePago credito;
+	private LocalDate fecha1;
+	private PrecioTemporal precioTemporal;
+	private ArrayList <PrecioTemporal> temporadasEspeciales;
 	
 	@BeforeEach
 	void setUp() throws Exception {
-		this.fechaDeIngreso = "2022-01-30";
-		this.fechaDeSalida = "2022-03-15";
+		this.fecha1 = LocalDate.parse("2022-01-15");
+		this.fechaDeIngreso = "2022-01-28";
+		this.fechaDeSalida = "2022-01-30";
 		this.fechaDeIngresoDate = LocalDate.parse(fechaDeIngreso);
 		this.fechaDeSalidaDate = LocalDate.parse(fechaDeSalida);
 		this.estadoReservaAceptada = mock(Aceptada.class);
 		this.inquilino = mock(Usuario.class);
 		this.publicacion = mock(Publicacion.class);
 		this.propietario = mock(Usuario.class);
-		this.reserva = new Reserva(inquilino, fechaDeIngresoDate, fechaDeSalidaDate, estadoReservaAceptada, publicacion);		
+		this.credito = mock(Credito.class);
+		this.precioTemporal = mock(PrecioTemporal.class);
+		this.temporadasEspeciales = new ArrayList<PrecioTemporal>();
+		this.reserva = new Reserva(fecha1, inquilino, fechaDeIngresoDate, fechaDeSalidaDate, estadoReservaAceptada, publicacion, credito);		
 	}
 	
 	@Test
@@ -40,36 +50,78 @@ class ReservaTestCase {
 	
 	@Test
 	void testReservaTieneFechaDeIngreso() {
-		assertEquals(this.fechaDeIngresoDate,this.reserva.getFechaDeIngreso());
+		LocalDate fechaDeIngresoDeReserva = this.reserva.getFechaDeIngreso();
+		assertEquals(this.fechaDeIngresoDate,fechaDeIngresoDeReserva);
 	}
 	
 	@Test
 	void testReservaTieneFechaDeSalida() {
-		assertEquals(this.fechaDeSalidaDate,this.reserva.getFechaDeSalida());
+		LocalDate fechaDeSalidaDeReserva = this.reserva.getFechaDeSalida();
+		assertEquals(this.fechaDeSalidaDate,fechaDeSalidaDeReserva);
 	}
 	
 	@Test
 	void testDiferenciaDeDiasEnReserva() {
-		assertEquals(44, this.reserva.getDiferenciaDeDias());
+		int diferenciaDeDias = this.reserva.getDiferenciaDeDias();
+		assertEquals(2, diferenciaDeDias);
+	}
+	
+
+	@Test
+	void testReservaTieneUnInquilino() {
+		Usuario inquilinoDeReserva = this.reserva.getInquilino();
+		assertEquals(this.inquilino, inquilinoDeReserva);
+	}
+	
+	@Test
+	void testReservaTieneUnPropietarioDePublicacion(){
+		when(this.publicacion.getPropietario()).thenReturn(this.propietario);
+		Usuario propietarioDeReserva = this.reserva.getPropietario();
+		assertEquals(this.propietario, propietarioDeReserva);
+		verify(this.publicacion).getPropietario();
+	}
+	
+	@Test
+	void testReservaTieneFormaDePagoCredito() {
+		when(this.credito.getTipo()).thenReturn("Credito");
+		String tipoDePago = this.reserva.getFormaDePago().getTipo();
+		assertEquals("Credito", tipoDePago);
+	}
+	
+	@Test
+	void testDiasRestantesHastaReserva() {
+		int diasRestantes = this.reserva.diasQueFaltan();
+		assertEquals(13, diasRestantes );
+	}
+	
+	@Test
+	void testCostoPorDiaEnTemporada() {
+		when(this.precioTemporal.getInicio()).thenReturn(LocalDate.parse("2020-06-20"));
+		when(this.precioTemporal.getFinal()).thenReturn(LocalDate.parse("2023-06-20"));
+		when(this.precioTemporal.getPrecio()).thenReturn(2500.00);
+		when(this.publicacion.getTemporadasEspeciales()).thenReturn(this.temporadasEspeciales);
+		this.temporadasEspeciales.add(precioTemporal);
+		float resultado = reserva.precioEnElDia(LocalDate.now());
+		assertEquals(2500.00f, resultado);
+	}
+	
+	@Test
+	void testCostoPorDiaFueraDeTemporada() {
+		when(this.publicacion.getPrecioBase()).thenReturn(2000.00f);
+		float resultado = reserva.precioEnElDia(LocalDate.now());
+		assertEquals(2000.00f, resultado);
+	}
+	
+	@Test
+	void testCostoTotalAlquilando2Dias() {
+		when(this.publicacion.getPrecioBase()).thenReturn(2000.00f);
+		float resultado = this.reserva.costoTotal();
+		assertEquals(4000.00f, resultado);
 	}
 	
 	@Test
 	void testEstadoDeReservaAceptada() {
 		this.reserva.setEstado(estadoReservaAceptada);
 		assertEquals(this.estadoReservaAceptada, this.reserva.getEstadoDeReserva());
-	}
-	
-	@Test
-	void testReservaTieneUnInquilino() {
-		assertEquals(this.inquilino, this.reserva.getInquilino());
-	}
-	
-	@Test
-	void testReservaTieneUnPropietarioDePublicacion(){
-		when(this.publicacion.getPropietario()).thenReturn(this.propietario);
-		assertEquals(this.propietario, this.reserva.getPropietario());
-		verify(this.publicacion).getPropietario();
-	}
-	
-	
+	}	
 }
