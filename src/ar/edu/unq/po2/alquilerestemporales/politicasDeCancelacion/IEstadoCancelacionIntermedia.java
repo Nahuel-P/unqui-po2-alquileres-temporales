1110ce64363bff18dcd45c3588ed90e2ecabf6da
Julian Vazquez <73852125+JuliVazquez@users.noreply.github.com>
2021-07-14 17:20:21 -0300

    borrado de clases que no van mas

--- src/ar/edu/unq/po2/alquilerestemporales/politicasDeCancelacion/IEstadoCancelacionIntermedia.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unq.po2.alquilerestemporales.politicasDeCancelacion;
-
-import ar.edu.unq.po2.alquilerestemporales.reserva.Reserva;
-
-public interface IEstadoCancelacionIntermedia {
-
-	void aplicarCosto(Reserva reserva, CancelacionIntermedia cancelacionIntermedia);
-
-}
